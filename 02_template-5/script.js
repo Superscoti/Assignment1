5dd4883ca35f4513b6b3b6ac9eac6f2833b7f011
Superscoti <57096064+Superscoti@users.noreply.github.com>
2020-04-03 21:17:55 +0200

    finished f2, and f4, f3 tilted so hard that i quit

--- 02_template-5/script.js
+++ 02_template-5/script.js
@@ -1,3 +1,4 @@
+/*pre-loading animation*/
 window.addEventListener("load", setTimeoutFunction);
 
 function setTimeoutFunction() {
@@ -8,6 +9,7 @@ function hideLoader() {
   const loader = document.querySelector(".loader").classList.add("hidden");
 }
 
+/*toggle Dark Mode*/
 function toggleDarkMode() {
    var darkMode = document.body;
    darkMode.classList.toggle("dark-mode-button");
@@ -18,4 +20,30 @@ function toggleLightMode() {
   var lightMode = document.body;
   lightMode.classList.toggle("dark-mode-button");
   document.getElementById('wrapper-darkmode').id = 'wrapper';
+}
+
+/*Slideshow*/
+
+
+/*Live-Clock*/
+function startTime() {
+
+  var liveTime = new Date();
+  var hours = liveTime.getHours();
+  var months = liveTime.getMinutes();
+  var second = liveTime.getSeconds();
+
+  months = liveTimeCheck(months);
+  second = liveTimeCheck(second);
+
+  document.getElementById('dateText').innerHTML =
+  hours + ":" + months + ":" + second;
+
+  var time = setTimeout(startTime, 500);
+}
+function liveTimeCheck(i) {
+  if (i < 10) {
+    i = "0" + i
+  };
+  return i;
 }
